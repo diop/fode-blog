ffcdbf925d683e2f4565fd5550e20429801e9ea8
diop <fodesdiop@gmail.com>
2021-06-09 17:22:14 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+### Fodé Diop's Official Blog
+A collection of essays in order to document my Bitcoin tinkerings and other explorations.
+
+© Cooyright 2021 Fodé Diop - MIT License 
---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+def main():
+    '''
+    Blog by Fodé Diop
+    '''
+
+    st.title('Tinkering')
+    st.text('by Fodé Diop')
+
+if __name__ == '__main__':
+    main()
